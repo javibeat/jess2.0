e48b233d291c2532b9b1012246e61b9b36128486
javibeat <javibeat@gmail.com>
2024-01-05 20:13:31 +0400

    menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,26 +2,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.querySelector(".hamburger");
     const navMenu = document.querySelector(".nav-menu");
 
-    // Event listener para el menú hamburguesa
     hamburger.addEventListener("click", () => {
         hamburger.classList.toggle("active");
         navMenu.classList.toggle("active");
     });
 
-    // Event listeners para los enlaces del menú
     document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", function(e) {
         const nextElement = this.nextElementSibling;
-        if (nextElement && nextElement.classList.contains('dropdown')) {
-            // Previene la navegación predeterminada si es un enlace de desplegable
+        if (hamburger.classList.contains("active") && nextElement && nextElement.classList.contains('dropdown')) {
             e.preventDefault();
-            // Muestra u oculta el menú desplegable
-            const isDropdownVisible = nextElement.style.display === 'block';
-            // Oculta cualquier desplegable abierto
+            const isVisible = nextElement.style.display === 'block';
             document.querySelectorAll('.dropdown').forEach(dropdown => dropdown.style.display = 'none');
-            // Muestra u oculta el desplegable actual
-            nextElement.style.display = isDropdownVisible ? 'none' : 'block';
+            nextElement.style.display = isVisible ? 'none' : 'block';
         } else {
-            // Si no es un desplegable, cierra el menú hamburguesa
             hamburger.classList.remove("active");
             navMenu.classList.remove("active");
         }
